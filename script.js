2ebf28110875a5012aaa953bf3b009a2236ea283
kazi-kabir <kazi.s221999@gmail.com>
2022-12-19 20:20:57 +1100

    add forms

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+let kilojoule = document.getElementById("kilojoule").value
+
 function convertKjtoKcal(num) {
     return num/4.184;
 }
 
+console.log(kilojoule)
 console.log(convertKjtoKcal(1000));
