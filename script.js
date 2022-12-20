a2c6a150fe587cd7b88104bee6de9cf74c4354c8
kazi-kabir <kazi.s221999@gmail.com>
2022-12-20 23:09:15 +1100

    getting value

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-let kilojoule = document.getElementById("kilojoule").value
+
+function myFunction() {
+    let kilojoule = document.getElementById("kilojoule").value
+    console.log(kilojoule);
+}
 
 function convertKjtoKcal(num) {
     return num/4.184;
 }
 
-console.log(kilojoule)
 console.log(convertKjtoKcal(1000));
