d405fb8df5fe1e954250dc371b8eec7b254a0009
kazi-kabir <kazi.s221999@gmail.com>
2022-12-22 21:14:14 +1100

    make it work when pressing enter

--- script.js
+++ script.js
@@ -4,8 +4,23 @@ function convertKjtoKcal(num) {
     return calorie
 }
 
+let kj = document.getElementById("kilojoule");
+
+kj.addEventListener("keydown", function (e) {
+    if (e.code === "Enter") {  //checks whether the pressed key is "Enter"
+            let valueOfKj = document.getElementById("kilojoule").value
+            let res = convertKjtoKcal(valueOfKj);
+            console.log(res) 
+
+        }
+});
+
+
 function myFunction() {
     let kilojoule = document.getElementById("kilojoule").value;
     console.log(convertKjtoKcal(kilojoule));
+
+
+
 }
 
